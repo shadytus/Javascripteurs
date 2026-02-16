4395e37c60e7e91e7030e7c7a003b26b63674d9f
vanessalynd <valynon3@gmail.com>
2026-02-16 20:49:02 +0100

    Add configuration constants for app settings

--- Web project (L2 PHP)/app/config/app.php
+++ Web project (L2 PHP)/app/config/app.php
@@ -1,1 +1,9 @@
+<php
+const ROOT_DIR = "../app/";
+const AJAX_ENABLED = true;
+const MEDIA_PATH = './media/';
 
+// Base de données
+const DATABASE_DSN      = 'mysql:host=localhost;dbname=press_2025_v04;charset=utf8';
+const DATABASE_USERNAME = 'root';
+const
