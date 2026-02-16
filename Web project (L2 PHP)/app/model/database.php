53761977b49b1eef8b5f5b11038c3c3f4f88d3d2
vanessalynd <valynon3@gmail.com>
2026-02-16 20:54:17 +0100

    Implement database connection and query functions
    Added database functions for PDO connection and queries.

--- Web project (L2 PHP)/app/model/database.php
+++ Web project (L2 PHP)/app/model/database.php
@@ -1,1 +1,28 @@
+<?php
+function db_get_pdo()
+{
+    static $pdo;
+    if( empty($pdo) )
+    {
+        $pdo = new PDO( DATABASE_DSN, DATABASE_USERNAME, DATABASE_PASSWORD );
+    }
+    return $pdo;
+}
 
+function db_select($query_s): array
+{
+    $pdo = db_get_pdo();
+    $stmt = $pdo->query($query_s);
+    $content_a = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    return $content_a;
+}
+
+function db_select_prepare($query_s, $param_a): array
+{
+    $pdo = db_get_pdo();
+    $stmt = $pdo->prepare($query_s);
+    $stmt->execute($param_a);
+    $content_a = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    return $content_a;
+}
+```
