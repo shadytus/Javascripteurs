0128bafad72e0d751a25782fe11a16706915b0a8
vanessalynd <valynon3@gmail.com>
2026-02-16 21:03:36 +0100

    Implement MVC file inclusion and page handling
    Add function to include MVC PHP files dynamically and handle page requests.

--- Web project (L2 PHP)/public/index.php
+++ Web project (L2 PHP)/public/index.php
@@ -1,1 +1,23 @@
+<?php
+require_once "../app/config/app.php";
 
+function include_mvc_php_files()
+{
+    foreach ( array('model', 'view', 'controller') as $dir )
+    {
+        $file_a = scandir(ROOT_DIR.$dir);
+        foreach ( $file_a as $file )
+        {
+            if( substr($file, -4, 4) != ".php" ) continue;
+            require_once( ROOT_DIR.$dir.DIRECTORY_SEPARATOR.$file );
+        }
+    }
+}
+
+session_start();
+include_mvc_php_files();
+
+$page = @$_REQUEST['page'] ?: 'home';
+$main = "main_{$page}";
+echo $main();
+```
