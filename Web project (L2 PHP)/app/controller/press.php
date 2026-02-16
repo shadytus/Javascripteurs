e68400a02bd54e411d28d3c60146bc827073458b
vanessalynd <valynon3@gmail.com>
2026-02-16 21:27:56 +0100

    Implement main_press function to display articles

--- Web project (L2 PHP)/app/controller/press.php
+++ Web project (L2 PHP)/app/controller/press.php
@@ -1,1 +1,25 @@
+<?php
+function main_press(): string
+{
+    $menu_a = get_menu();
+    $articles_a = press_get_articles();
 
+    $html = '';
+    foreach ($articles_a as $article) {
+        $html .= <<<HTML
+        <article class="article-card">
+            <h3>{$article['title_art']}</h3>
+            <p class="accroche">{$article['accroche_art']}</p>
+            <a href="index.php?page=article&id={$article['id_art']}">Lire la suite</a>
+            <button class="btn-favori" data-id="{$article['id_art']}">⭐ Favori</button>
+        </article>
+        HTML;
+    }
+
+    return join("\n", [
+        html_head($menu_a),
+        "<h2>Tous les articles</h2>",
+        "<div class='articles-list'>{$html}</div>",
+        html_foot(),
+    ]);
+}
