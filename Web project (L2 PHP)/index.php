e4970e151e8404fed5342bf7e1fba2fa9625dfa1
vanessalynd <valynon3@gmail.com>
2026-02-20 20:32:01 +0100

    Refactor include_mvc_php_files function

--- Web project (L2 PHP)/index.php
+++ Web project (L2 PHP)/index.php
@@ -6,7 +6,19 @@ require_once "../app/controller/login.php";
 
 function include_mvc_php_files()
 {
-@@ -19,5 +22,7 @@ function include_mvc_php_files()
+    foreach ( array('model', 'view', 'controller') as $dir )
+    {
+        $file_a = scandir(ROOT_DIR.$dir);
+        foreach ( $file_a as $file )
+        {
+            if( substr($file, -4, 4) != ".php" ) continue;
+            require_once( ROOT_DIR.$dir.DIRECTORY_SEPARATOR.$file );
+        }
+    }
+}
+
+session_start();
+include_mvc_php_files();
 
 $page = @$_REQUEST['page'] ?: 'home';
 $main = "main_{$page}";
