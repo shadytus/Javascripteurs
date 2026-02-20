353c20934f686e6e0de6166a5adf03b7fbb0fa74
vanessalynd <valynon3@gmail.com>
2026-02-20 19:14:49 +0100

    Implement favorites management functions

--- Web project (L2 PHP)/app/model/favorites.php
+++ Web project (L2 PHP)/app/model/favorites.php
@@ -1,1 +1,20 @@
+<?php
+function favorites_get(): array
+{
+    return $_SESSION['favoris'] ?? [];
+}
 
+function favorites_add(int $id): void
+{
+    $_SESSION['favoris'][$id] = $id;
+}
+
+function favorites_remove(int $id): void
+{
+    unset($_SESSION['favoris'][$id]);
+}
+
+function favorites_clear(): void
+{
+    $_SESSION['favoris'] = [];
+}
