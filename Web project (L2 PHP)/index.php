dd6d8cfd0002f3b5c5d028487a6e344ac0a5526a
vanessalynd <valynon3@gmail.com>
2026-02-20 20:30:58 +0100

    Include MVC files for favorites and login
    Added MVC file inclusions for favorites and login functionality.

--- Web project (L2 PHP)/index.php
+++ Web project (L2 PHP)/index.php
@@ -1,1 +1,16 @@
+<?php
+require_once "../app/config/app.php";
+require_once "../app/model/favorites.php";
+require_once "../app/controller/favoris.php";
+require_once "../app/controller/login.php";
 
+function include_mvc_php_files()
+{
+@@ -19,5 +22,7 @@ function include_mvc_php_files()
+
+$page = @$_REQUEST['page'] ?: 'home';
+$main = "main_{$page}";
+$favoris = "main_{favoris}";
+$login = "main_{login}";
+$logout = "main_{logout}";
+echo $main();
