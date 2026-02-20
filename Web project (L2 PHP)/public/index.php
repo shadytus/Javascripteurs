28c3c5bc9dd892d73ce69ae7f04fe15126dfd7dc
vanessalynd <valynon3@gmail.com>
2026-02-20 20:20:30 +0100

    Add additional requires for favorites and login

--- Web project (L2 PHP)/public/index.php
+++ Web project (L2 PHP)/public/index.php
@@ -1,5 +1,8 @@
 <?php
 require_once "../app/config/app.php";
+require_once "../app/model/favorites.php";
+require_once "../app/controller/favoris.php";
+require_once "../app/controller/login.php";
 
 function include_mvc_php_files()
 {
@@ -19,5 +22,7 @@ include_mvc_php_files();
 
 $page = @$_REQUEST['page'] ?: 'home';
 $main = "main_{$page}";
+$favoris = "main_{favoris}";
+$login = "main_{login}";
+$logout = "main_{logout}";
 echo $main();
-```
