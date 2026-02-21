c7fef83a2267c68c081e71a76e73f0a5b9cb838b
vanessalynd <valynon3@gmail.com>
2026-02-21 01:19:17 +0100

    Refactor database functions for consistency and clarity

--- Web project (L2 PHP)/app/model/database.php
+++ Web project (L2 PHP)/app/model/database.php
@@ -2,27 +2,24 @@
 function db_get_pdo()
 {
     static $pdo;
-    if( empty($pdo) )
+    if (empty($pdo))
     {
-        $pdo = new PDO( DATABASE_DSN, DATABASE_USERNAME, DATABASE_PASSWORD );
+        $pdo = new PDO(DATABASE_DSN, DATABASE_USERNAME, DATABASE_PASSWORD);
     }
     return $pdo;
 }
 
-function db_select($query_s): array
+function db_select(string $query_s): array
 {
-    $pdo = db_get_pdo();
-    $stmt = $pdo->query($query_s);
-    $content_a = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    return $content_a;
+    $pdo    = db_get_pdo();
+    $stmt   = $pdo->query($query_s);
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 
-function db_select_prepare($query_s, $param_a): array
+function db_select_prepare(string $query_s, array $param_a): array
 {
-    $pdo = db_get_pdo();
+    $pdo  = db_get_pdo();
     $stmt = $pdo->prepare($query_s);
     $stmt->execute($param_a);
-    $content_a = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    return $content_a;
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
-```
